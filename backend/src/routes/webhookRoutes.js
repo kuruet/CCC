fcb32d10862551325e714f3be7bee65619db0506
kuruet <bpratik728@gmail.com>
2026-02-01 11:12:33 +0530

    feat: switch email delivery to Resend and finalize production email flow

--- backend/src/routes/webhookRoutes.js
+++ backend/src/routes/webhookRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { razorpayWebhookHandler } from "../controllers/razorpayWebhookController.js";
+import { razorpayWebhookHandler } from "../controllers/webhookController.js";
 
 const router = express.Router();
 
